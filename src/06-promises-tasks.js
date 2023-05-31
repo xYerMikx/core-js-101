5fc892cc1def16217ccb29c90d075545116c38a0
xYerMikx <yermik2014@yandex.ru>
2023-05-31 16:19:26 +0300

    feat: solve all 06-promises-tasks and 05-objects-tasks(except 1)

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,12 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer !== 'boolean') reject(new Error('Wrong parameter is passed! Ask her again.'));
+    if (isPositiveAnswer) resolve('Hooray!!! She said "Yes"!');
+    resolve('Oh no, she said "No".');
+  });
 }
 
 
@@ -48,8 +52,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array).then((values) => [...values]);
 }
 
 /**
@@ -71,8 +75,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array).then((value) => value);
 }
 
 /**
@@ -92,8 +96,32 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  return new Promise((resolve) => {
+    let count = 0;
+    const results = [];
+
+    array.forEach((promise) => {
+      promise
+        .then((value) => {
+          results.push(value);
+          count += 1;
+
+          if (count === array.length) {
+            const result = results.reduce(action);
+            resolve(result);
+          }
+        })
+        .catch(() => {
+          count += 1;
+
+          if (count === array.length) {
+            const result = results.reduce(action);
+            resolve(result);
+          }
+        });
+    });
+  });
 }
 
 module.exports = {
